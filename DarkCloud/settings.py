fcac149ebcf0213c874c313ffdd42fdbe8deaa0c
lDarkManl <81003396+lDarkManl@users.noreply.github.com>
2024-03-20 01:56:41 +0700

    deleted middleware login

--- DarkCloud/settings.py
+++ DarkCloud/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'DarkCloud.middleware.AccessMiddleware',
 ]
 
 ROOT_URLCONF = 'DarkCloud.urls'
